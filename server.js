19f49a129b4108b12409fdad71df9b5e3aa8c261
Latifah Lillie <queen1892@gmail.com>
2021-03-01 14:19:34 -0600

    The homepage route is working and routing to the index.html file

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 let express = require("express");
+let path = require("path");
+
+let app = express();
+var PORT = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
