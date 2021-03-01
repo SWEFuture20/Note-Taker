b676633758f790f886735b788f42fdbd95788dde
Latifah Lillie <queen1892@gmail.com>
2021-03-01 15:03:21 -0600

    Adding the Get Route for Notes to connect to the db.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let path = require("path");
 let fs = require("fs")
+let db = require("./db/db.json")
 
 let app = express();
 var PORT = 5000;
@@ -8,9 +9,20 @@ var PORT = 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
+
+app.get("/notes", function(req,res){
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  // res.render("./public/notes.html")
+})
+
+app.get("/api/notes", function(req,res){
+  // res.json(db)
+})
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
