7b82e2d978f9849d19c850994232bc210ad99d4c
Latifah Lillie <queen1892@gmail.com>
2021-03-02 21:49:20 -0600

    Updated CSS

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Note-Taker
 
 In this app, I will be displaying how to create and delete notes on the backend with express and JSON. I will be using JSON for a database, HTML, CSS, and Javascript. Enjoy the App!
+
+# Explanation
+
+A user will be able to 
--- server.js
+++ server.js
@@ -4,20 +4,20 @@ let path = require("path");
 let fs = require("fs")
 let db = require("./db/db.json")
 // Establishing the Router and the Port Number
-let app = express();
-var PORT = 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 // 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// This is the GET route that will display the notes html file
 app.get("/notes", function(req,res){
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
-
+// This is the GET route that is displaying the index.html as the home page
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
+// This is the GET route that is reading the information that is store in the save object
 app.get("/api/notes", function(req,res){
   fs.readFile(db, (err,data) => {
     if (err) throw err;
@@ -26,13 +26,15 @@ app.get("/api/notes", function(req,res){
   })
   
 })
-
+// This is A POST route that is going to create new objects inside of the db.json
+// This is the POST route that is reading the information in the db.json and its going to store the save object and push additional object afterwards.
 app.post("/api/notes", function(req,res){
   fs.readFile(db, (err,data) => {
     if (err) throw err;
     var save = JSON.parse(data)
     save.push(req.body)
     save = JSON.stringify(save)
+    // The writeFile storing the new save object into the db.json. 
     fs.writeFile(db, save, (err) => {
       if (err) throw err;
       var save = JSON.parse(save)
@@ -42,6 +44,8 @@ app.post("/api/notes", function(req,res){
   
 })
 
+// This is the DELETE route and its reading whats stored in the db.json
+// THe save object is going to read all notes in db.json and remove notes with a specific id and rewrite notes to the db.json
 app.delete("/api/notes/:id", function(req,res){
   fs.readFile(db, (err,data) => {
     if (err) throw err;
